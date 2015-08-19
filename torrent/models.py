96a742d5ae7c398c4717e6c62be269c3affd379a
Kartavtsev Sergey <crazyserega1994@mail.ru>
2015-08-19 13:30:53 +0300

    Added torrent model

--- torrent/models.py
+++ torrent/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Torrent(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    file_size = models.IntegerField()
