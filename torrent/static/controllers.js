55679981a1707a8e3da8b48c69908eb1beaef06c
Kartavtsev Sergey <crazyserega1994@mail.ru>
2015-08-27 23:08:57 +0300

    Added auto torrent updater every 1 second. Added basic sort function. Added bytes to kb/gb/tb converter

--- torrent/static/controllers.js
+++ torrent/static/controllers.js
@@ -1,8 +1,24 @@
 var controllers = angular.module('torrentControllers', ['torrentFactories']);
 
-controllers.controller('TorrentsController', ['$scope', 'torrentFactory', function ($scope, torrentFactory) {
+controllers.controller('TorrentsController', ['$scope', '$interval', 'torrentFactory', function ($scope, $interval, torrentFactory) {
 
+    var updateTimer;
+    $scope.sortType = '2';
+    $scope.sortReverse = false;
 
+    $scope.bytesToSize = function (bytes) {
+        var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+        if (bytes == 0) return 'n/a';
+        var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+        if (i == 0) return bytes + ' ' + sizes[i];
+        return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+    };
+
+    $scope.startUpdater = function () {
+        updateTimer = $interval(function () {
+            $scope.getCustomers();
+        }, 1000);
+    };
 
     $scope.getCustomers = function () {
         torrentFactory.getTorrents().success(function (data) {
@@ -11,5 +27,7 @@ controllers.controller('TorrentsController', ['$scope', 'torrentFactory', functi
 
         });
     };
+
     $scope.getCustomers();
+    $scope.startUpdater();
 }]);
